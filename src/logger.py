d4b52a017160b686457fd9b4d0119a6dc2061284
nasir015 <nasir1515@student.nstu.edu.bd>
2023-09-22 18:24:46 +0600

    5

--- src/logger.py
+++ src/logger.py
@@ -25,17 +25,24 @@ if __name__ == "__main__":
 
 import logging as lg
 
-lg.basicConfig(filename= 'logs\\file_handle\\log.txt',
+def logger(file_name , massage):
+    lg.basicConfig(filename= file_name,
                level=lg.INFO,format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",filemode='a+')
-console_log = lg.StreamHandler()
-console_log.setLevel(lg.INFO)
-format_1 =  lg.Formatter("[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s")
-console_log.setFormatter(format_1)
-lg.getLogger('').addHandler(console_log)
-logger1 = lg.getLogger('Nasir')
+    console_log = lg.StreamHandler()
+    console_log.setLevel(lg.INFO)
+    format_1 =  lg.Formatter("[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s")
+    console_log.setFormatter(format_1)
+    lg.getLogger('').addHandler(console_log)
+    logger1 = lg.getLogger('Nasir')
+    logger1.info(massage)
 
 
-logger1.info("Starting")
+
+
+
+
+
+
 
 
 
